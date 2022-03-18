60e1a5b1b9183ade4f97ab3eefe70a75c783914b
Maverick Leroux <78061003+geekmaster21@users.noreply.github.com>
2022-03-18 00:12:41 -0400

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-DEFCONFIG=arch/arm64/configs/a71_defconfig
+git clone https://github.com/proprietary-stuff/llvm-arm-toolchain-ship-10.0.git llvm-sdclang
+git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git gcc
+
+DEFCONFIG=a71_defconfig
 
 # Declare your CLANG n GCC Location HERE!
 CLANG_PATH=/workspace/a71-android12/llvm-sdclang
@@ -10,12 +13,14 @@ export ANDROID_MAJOR_VERSION=R
 
 make O=out ARCH=arm64 $DEFCONFIG
 
-PATH="$CLANG_PATH/bin:${PATH}" \
-                      make -j8 O=out $KERNEL_MAKE_ENV \
+PATH="$CLANG_PATH/bin:$GCC_PATH/bin:${PATH}" \
+                      make O=out $KERNEL_MAKE_ENV \
                       ARCH=arm64 \
                       CC=clang \
                       LD=ld.lld \
-                      CROSS_COMPILE=aarch64-linux-gnu- \
+                      CROSS_COMPILE=aarch64-linux-android- \
+                      CLANG_TRIPLE=aarch64-linux-gnu- \
+                      AR=llvm-ar \
                       NM=llvm-nm \
                       OBJCOPY=llvm-objcopy \
                       OBJDUMP=llvm-objdump \
